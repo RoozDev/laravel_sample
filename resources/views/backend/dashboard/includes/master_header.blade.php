e6d28f855715b6dc3f0976f79f085b8accd8fe9e
seducerooo <>
2023-10-27 16:10:00 +0330

    in views/backend/dashboard directory includes directory created && in views/backend/dashboard/includes directory master_footer and master_footer_section and master_header and master_right_sidebar blade file created

---
+++ resources/views/backend/dashboard/includes/master_header.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="rtl">
+
+<head>
+
+    <meta charset="utf-8" />
+    <title>Roozbeh Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Coderthemes" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.ico') }}">
+    <!-- Plugins css -->
+    <link href="{{ asset('backend/assets/libs/flatpickr/flatpickr.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('backend/assets/libs/selectize/css/selectize.bootstrap3.css') }}" rel="stylesheet" type="text/css" />
+
+    <!-- Bootstrap css -->
+    <link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('backend/assets/css/bootstrap.css') }}" rel="stylesheet" type="text/css" />
+    <!-- App css -->
+    <link href="{{ asset('backend/assets/css/app-rtl.min.css') }}" rel="stylesheet" type="text/css" id="app-style"/>
+
+    <!-- icons -->
+    <link href="{{ asset('backend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- Head js -->
+    <script src="{{ asset('backend/assets/js/head.js') }}"></script>
+
+</head>
