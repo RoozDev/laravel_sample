6ee726b66d57296e61fa71cb50539e2108de6649
seducerooo <>
2023-10-27 13:20:01 +0330

    vite removed && laravel-mix installed and compiled successfully && bootstrap layouts installed and compiled successfully && in views/layouts directory  master_backend_project created and its assets replaced && in resources/views directory backend directory created && in views/backend directory dashboard directory created && in views/backend/dashboard directory index blade file created && in public directory backend directory created && in public/backend directory project assets added && DashboardController created successfully

---
+++ resources/views/backend/dashboard/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master_backend_project')
+@section('content')
+    <!-- Start container-fluid  -->
+    <div class="container-fluid">
+        hello world
+    </div>
+    <!-- end container-fluid -->
+@endsection
